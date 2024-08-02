cb1cef2602cfe39058ba6d9124cae746b8daf301
Jacobss1 <168560411+Jacobss1@users.noreply.github.com>
2024-08-02 15:26:39 +0800

    Update SampleSnene.unity

--- Assets/Scripts/Player/CameraFollow.cs
+++ Assets/Scripts/Player/CameraFollow.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    private GameObject player1;
+    private GameObject Player;
     [SerializeField] float timeOffset = 0.3f;
     [SerializeField] Vector3 posOffset = new Vector3(0, 0, -10); // Ensure the camera z position is correct
     [SerializeField] float leftLimit;
@@ -15,16 +15,16 @@ public class CameraFollow : MonoBehaviour
 
     private void Start()
     {
-        player1 = GameObject.FindGameObjectWithTag("Player");
+        Player = GameObject.FindGameObjectWithTag("Player");
     }
 
     void Update()
     {
-        if (player1 == null)
+        if (Player == null)
         {
-            player1 = GameObject.FindGameObjectWithTag("Player");
+            Player = GameObject.FindGameObjectWithTag("Player");
         }
-        if (player1 != null)
+        if (Player != null)
         {
             CameraFollowToPlayerWithCornerBoundary();
         }
@@ -33,7 +33,7 @@ public class CameraFollow : MonoBehaviour
     void CameraFollowToPlayerWithCornerBoundary()
     {
         Vector3 startPos = transform.position;
-        Vector3 endPos = player1.transform.position + posOffset; // Apply offset directly
+        Vector3 endPos = Player.transform.position + posOffset; // Apply offset directly
         transform.position = Vector3.SmoothDamp(startPos, endPos, ref velocity, timeOffset);
 
         // Clamp camera position within limits

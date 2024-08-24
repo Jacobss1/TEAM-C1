0c4931638f89d39e5a0a1316912fa4b444a009dc
Jacobss1 <168560411+Jacobss1@users.noreply.github.com>
2024-08-24 23:09:49 +0800

    plaerUpdate

--- Assets/Scripts/Interactables/Trampoline.cs
+++ Assets/Scripts/Interactables/Trampoline.cs
@@ -5,24 +5,27 @@ using UnityEngine;
 public class Trampoline : MonoBehaviour
 {
     public float jumpBoost;
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            Rigidbody2D rb2d = GetComponent<Rigidbody2D>();
+            Rigidbody2D rb2d = collision.gameObject.GetComponent<Rigidbody2D>();
+            Animator animator = collision.gameObject.GetComponent<Animator>();
+
             if (rb2d != null)
             {
-                rb2d.velocity = new Vector2(rb2d.velocity.x, jumpBoost);
+                // Reset the player's vertical velocity before applying the jump boost
+                rb2d.velocity = new Vector2(rb2d.velocity.x, 0f);
+
+                // Apply an upward force to simulate a trampoline bounce
+                rb2d.AddForce(Vector2.up * jumpBoost, ForceMode2D.Impulse);
+
+                // Play the mid air animation
+                if (animator != null)
+                {
+                    animator.Play("mid-air");
+                }
             }
         }
     }
